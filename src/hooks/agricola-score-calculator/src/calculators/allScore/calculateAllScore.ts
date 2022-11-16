f44dc1fd256032d52956bfaf7eff6ea32d1e1c3d
Masayuki Onishi <47713499+masayuki-0319@users.noreply.github.com>
2022-11-16 11:30:46 +0900

    a

--- src/hooks/agricola-score-calculator/src/calculators/allScore/calculateAllScore.ts
+++ src/hooks/agricola-score-calculator/src/calculators/allScore/calculateAllScore.ts
@@ -2,19 +2,19 @@ import { ScoreResource } from '../../types';
 import {
   calculateFamily,
   calculateCard,
-  calculateFarms,
+  calculateFarmFacility,
   calculateProducts,
 } from '../categoryScore';
 
 export function calculateResult(scoreResource: ScoreResource): number {
   const { farm, family, card, products } = scoreResource;
 
-  const farmScore = calculateFarms(farm);
+  const farmFacilityScore = calculateFarmFacility(farm);
   const familyScore = calculateFamily(family);
   const cardScore = calculateCard(card);
   const productsScore = calculateProducts(products);
 
-  const result = farmScore + familyScore + cardScore + productsScore;
+  const result = farmFacilityScore + familyScore + cardScore + productsScore;
 
   return result;
 }
